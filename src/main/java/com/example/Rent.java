50a3a7d14c773ae45ab3a296dd4c7915d3338cdd
AmamiSQ <58980413+AmamiSQ@users.noreply.github.com>
2020-11-21 22:09:57 +0100

    Create Rent.java

---
+++ src/main/java/com/example/Rent.java
@@ -0,0 +1,85 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.contextmenu.SubMenu;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.menubar.MenuBar;
+import com.vaadin.flow.component.menubar.MenuBarVariant;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+@Route(value="Rent")
+@CssImport("./styles/styles.css")
+public class Rent extends VerticalLayout {
+    private static final long serialVersionUID = 1L;
+
+    public Rent() {
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        setSizeFull();
+        addClassName("rent");
+        addHeader();
+
+        H2 title = new H2("Car Overview");
+        title.getElement().getThemeList();
+    }
+
+    // HEADER
+    public void addHeader() {
+        // Header
+        H1 header = new H1("QARS");
+        header.getElement().getThemeList();
+        
+        // Menu bar 
+        MenuBar menuBar = new MenuBar(); 
+        menuBar.addClassName("menuBar");
+        menuBar.addThemeVariants(MenuBarVariant.LUMO_PRIMARY);
+        menuBar.setOpenOnHover(true);
+
+        // Menu bar - Main
+        MenuItem menuItemHome = menuBar.addItem("Home");
+        menuItemHome.addClickListener(e -> menuItemHome.getUI().ifPresent(ui -> ui.navigate("")));
+        MenuItem menuItemRent = menuBar.addItem("Rent");
+        MenuItem menuItemLocations = menuBar.addItem("Locations");
+        menuItemLocations.addClickListener(e -> menuItemLocations.getUI().ifPresent(ui -> ui.navigate("Locations")));
+        MenuItem menuItemFaq = menuBar.addItem("FAQ");
+        menuItemFaq.addClickListener(e -> menuItemFaq.getUI().ifPresent(ui -> ui.navigate("FAQ")));
+        MenuItem menuItemCorona = menuBar.addItem("COVID-19");
+        menuItemCorona.addClickListener(e -> menuItemCorona.getUI().ifPresent(ui -> ui.navigate("Corona")));
+        MenuItem menuItemLogin = menuBar.addItem("Login");
+        menuItemLogin.addComponentAsFirst(new Icon(VaadinIcon.USER));
+        menuItemLogin.addClickListener(e -> menuItemLogin.getUI().ifPresent(ui -> ui.navigate("Login")));
+
+        // Menu bar - Sub menu's 
+        SubMenu subMenuRent = menuItemRent.getSubMenu();
+        MenuItem menuItemRentACar = subMenuRent.addItem("Rent a car");
+        menuItemRentACar.addClickListener(e -> menuItemRentACar.getUI().ifPresent(ui -> ui.navigate("Rent")));
+        MenuItem menuItemRentInformation = subMenuRent.addItem("Information");
+        menuItemRentInformation.addClickListener(e -> menuItemRentInformation.getUI().ifPresent(ui -> ui.navigate("Information")));
+        MenuItem menuItemExtras = subMenuRent.addItem("Extra options");
+        menuItemExtras.addClickListener(e -> menuItemExtras.getUI().ifPresent(ui -> ui.navigate("Extras")));
+        
+        SubMenu subMenuLogin = menuItemLogin.getSubMenu();
+        MenuItem menuItemRegister = subMenuLogin.addItem("Register");
+        menuItemRegister.addClickListener(e -> menuItemRegister.getUI().ifPresent(ui -> ui.navigate("Register")));
+        add(header, menuBar);
+    }
+}
