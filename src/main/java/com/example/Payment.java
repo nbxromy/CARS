7258f204cb7e321986d32d31aad9d658cfadc22f
nbxromy <61189931+nbxromy@users.noreply.github.com>
2021-01-05 16:50:19 +0100

    Update Payment.java

--- src/main/java/com/example/Payment.java
+++ src/main/java/com/example/Payment.java
@@ -80,7 +80,7 @@ public class Payment extends VerticalLayout {
         month.setPlaceholder("Month");
         month.setItems(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
         year.setPlaceholder("Year");
-        year.setItems(20, 21, 22, 23, 24, 25);
+        year.setItems(21, 22, 23, 24, 25);
         submit.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         payOnLocation.addThemeVariants(ButtonVariant.LUMO_SUCCESS);
         buttonLayout.add(submit, payOnLocation, cancel);
