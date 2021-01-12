9645b363d22fbfb6f1fde3f2627840d4722efa33
nbxromy <61189931+nbxromy@users.noreply.github.com>
2021-01-12 10:03:51 +0100

    56t356y

---
+++ src/main/java/org/vaadin/examples/form/data/LocInfo.java
@@ -0,0 +1,29 @@
+package org.vaadin.examples.form.data;
+
+
+public class LocInfo{
+
+    private String countryCode;
+    private String city;
+    private String address;
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
